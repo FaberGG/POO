3ced44bcd75790add1f09d7fd8431d88a28598a7
faber navia <97317782+FaberGG@users.noreply.github.com>
2023-08-23 11:09:21 -0500

    Update name Poo_1.java
    Se me había olvidado colocar el nombre

--- Poo_1.java
+++ Poo_1.java
@@ -1,3 +1,4 @@
+John Faber Navia Narváez 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
