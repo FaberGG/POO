67daa5f2cc1a84a4d25b406c78cc862fefa463a1
faber navia <97317782+FaberGG@users.noreply.github.com>
2023-11-04 13:43:52 -0500

    Correcciones parcial Main.java
    Se elimino la opcion de ingresar los datos para cada trabajador, en su lugar se lleno la linkedlist con los objetos.
    Se mejoraron las impresiones en pantalla de cada informacion para hacer mas legible el seguimiento del programa.

--- ParcialPOO/parcialpoo/Main.java
+++ ParcialPOO/parcialpoo/Main.java
@@ -19,113 +19,122 @@ public class Main {
      */
     public static void main(String[] args) {
 
-//        Empleado empleado = new Empleado();
         Scanner input = new Scanner(System.in);
+        //creo la lista de objetos trabajadores
+        LinkedList<Empleado> objetos = new LinkedList<>();
 
-        LinkedList<Empleado> objetos = new LinkedList<Empleado>();
-        System.out.println("--CUANTOS TRABAJADORES DESEA INGRESAR AL SISTEMA?--");
-        int cantTrabajadores = input.nextInt();
-        
-        for (int i = 0; i < cantTrabajadores; i++) {
-            input.nextLine(); // consume la nueva linea antes de la nueva entrada
-            
-            System.out.println("ingrese el nombre del trabajador " + (i + 1));
-            String nombre = input.nextLine();
-            
-            System.out.println("¿Qué tipo de trabajador es? (1 para Asalariado, 2 para Por Horas)");
-            int tipoTrabajador = input.nextInt();
-            input.nextLine(); // Consume la nueva línea
-            
-            System.out.println("ingrese el salario del trabajador " + (i + 1));
-            Double salario = input.nextDouble();
-            
-            if (tipoTrabajador == 1) {
-                System.out.println("ingrese el las deducciones del trabajador " + (i + 1));
-                Double deducciones = input.nextDouble();
-                EmpleadoAsalariado empleadoA = new EmpleadoAsalariado(nombre, salario, deducciones);
-                objetos.add(i, empleadoA);
+         //se crean los objetos empleados
+        EmpleadoAsalariado empleado1 = new EmpleadoAsalariado("Juan", 800, 100);
+        EmpleadoAsalariado empleado2 = new EmpleadoAsalariado("Alberto", 1000, 250);
+        EmpleadoAsalariado empleado3 = new EmpleadoAsalariado("Maria", 1000, 200);
+        EmpleadoAsalariado empleado4 = new EmpleadoAsalariado("Camilo", 700, 200);
+        EmpleadoAsalariado empleado5 = new EmpleadoAsalariado("Estefania", 1200, 300);
+        EmpleadoPorHoras empleado6 = new EmpleadoPorHoras("Luis", 900, 35, 30);
+        EmpleadoPorHoras empleado7 = new EmpleadoPorHoras("Marcos", 600, 40, 20);
+        EmpleadoPorHoras empleado8 = new EmpleadoPorHoras("Teresa", 400, 35, 10);
+        EmpleadoPorHoras empleado9 = new EmpleadoPorHoras("Sara", 400, 45, 5);
+        EmpleadoPorHoras empleado10 = new EmpleadoPorHoras("Jhonatan", 450, 50, 5);
 
-            } else if(tipoTrabajador == 2){
-                System.out.println("ingrese el las horas trabajadas del trabajador " + (i + 1));
-                int horasTrabajadas = input.nextInt();
-                System.out.println("ingrese el valor por hora del trabajador " + (i + 1));
-                Double tarifaHora = input.nextDouble();
-                EmpleadoPorHoras empleadoH = new EmpleadoPorHoras(nombre, salario, horasTrabajadas, tarifaHora);
-                objetos.add(i, empleadoH);
-            }            
-        }
+        //se añaden los objetos a la lista
+        objetos.add(empleado1);
+        objetos.add(empleado2);
+        objetos.add(empleado3);
+        objetos.add(empleado4);
+        objetos.add(empleado5);
+        objetos.add(empleado6);
+        objetos.add(empleado7);
+        objetos.add(empleado8);
+        objetos.add(empleado9);
+        objetos.add(empleado10);
 
         //obtener empleados por hora
+        System.out.println("\n--------------------------------");
+        System.out.println("LOS EMPLEADOS POR HORA SON:");
+        System.out.println("--------------------------------");
+
         for (Empleado empleado : objetos) {
-            System.out.println("Los empleados por hora son:");
-            if (empleado instanceof EmpleadoPorHoras) {
-                EmpleadoPorHoras empleadoPorHoras = (EmpleadoPorHoras) empleado;
-                System.out.println("Trabajador por horas:");
-                System.out.println("Nombre: " + empleadoPorHoras.getNombre());
-                System.out.println("Salario: " + empleadoPorHoras.getSalario());
-                System.out.println("Horas trabajadas: " + empleadoPorHoras.getHorasTrabajadas());
-                System.out.println("Tarifa por hora: " + empleadoPorHoras.getTarifaHora());
+            if (empleado instanceof EmpleadoPorHoras empleadoPorHoras) {
+                System.out.println("\n---Trabajador por horas---");
+                System.out.println(" Nombre:               " + empleadoPorHoras.getNombre());
+                System.out.println(" Salario:              " + empleadoPorHoras.getSalario());
+                System.out.println(" Horas trabajadas:     " + empleadoPorHoras.getHorasTrabajadas());
+                System.out.println(" Tarifa por hora:      " + empleadoPorHoras.getTarifaHora());
             }
         }
         // obtener empleados por deducciones
+        System.out.println("\n--------------------------------");
+        System.out.println("LOS EMPLEADOS CON SALARIO FIJO SON:");
+        System.out.println("--------------------------------");
         for (Empleado empleado : objetos) {
-            System.out.println("Los empleados por salario fijo son son:");
-             else if (empleado instanceof EmpleadoAsalariado) {
-                EmpleadoAsalariado empleadoAsalariado = (EmpleadoAsalariado) empleado;
-                System.out.println("Trabajador con salario fijo:");
-                System.out.println("Nombre: " + empleadoAsalariado.getNombre());
-                System.out.println("Salario: " + empleadoAsalariado.getSalario());
-                System.out.println("Deducciones: " + empleadoAsalariado.getDeducciones());
+            if (empleado instanceof EmpleadoAsalariado empleadoAsalariado) {
+                System.out.println("\n---Trabajador con salario fijo---");
+                System.out.println(" Nombre:               " + empleadoAsalariado.getNombre());
+                System.out.println("Salario:               " + empleadoAsalariado.getSalario());
+                System.out.println("Deducciones:           " + empleadoAsalariado.getDeducciones());
             }
         }
         //Modificar el nombre o salario de un empleado
-        System.out.println("---MODIFICAR NOMBRE O SALARIO---");
-        System.out.println("De cual trabajador desea modificar el nombre o salario? ");
-        System.out.print("  Ingese el numero de trabajador: ");
-        int num = input.nextInt();
+        System.out.println("\n--------------------------------");
+        System.out.println("MODIFICAR NOMBRE O SALARIO");
+        System.out.println("--------------------------------");
+        System.out.println("\n De cual trabajador desea modificar el nombre o salario? ");
 
-        if (num < 0 || num >= objetos.size()) {
-            System.out.println("Número de trabajador fuera de rango. Por favor, ingrese un número válido.");
-        } else {
-            System.out.println("Que desea modificar NOMBRE[1] O SALARIO[2]");
-            int opc = input.nextInt();
-            input.nextLine(); // Consume la nueva línea
-            if (opc == 1) {
-                System.out.print(" \nIngrese el nombre nuevo del trabajador: ");
-                String nombre = input.nextLine();
-                objetos.get(num).setNombre(nombre);
-            } else if (opc == 2) {
-                System.out.print(" \nIngrese el salario nuevo del trabajador: ");
-                double salario = input.nextDouble();
-                objetos.get(num).setSalario(salario);
+        int num;
+        do {
+            System.out.print("  Ingese el numero de trabajador: ");
+            num = input.nextInt();
+            if (num < 0 || num >= objetos.size()) {
+                System.out.println("Numero de trabajador fuera de rango. Por favor, ingrese un numero valido.");
+            } else {
+                System.out.println("Que desea modificar NOMBRE[1] O SALARIO[2]");
+                int opc = input.nextInt();
+                input.nextLine(); // Consume la nueva línea
+                if (opc == 1) {
+                    System.out.print(" \nIngrese el nombre nuevo del trabajador: ");
+                    String nombre = input.nextLine();
+                    objetos.get(num).setNombre(nombre);
+
+                } else if (opc == 2) {
+                    System.out.print(" \nIngrese el salario nuevo del trabajador: ");
+                    double salario = input.nextDouble();
+                    objetos.get(num).setSalario(salario);
+                    input.nextLine(); // Consume la nueva línea
+
+                }
             }
-            input.nextLine(); // Consume la nueva línea
-        }
+        } while (num < 0 || num >= objetos.size());
 
-        // Obtener empleados por horas
+        //obtener empleados por hora
+        System.out.println("\n--------------------------------");
+        System.out.println("LOS EMPLEADOS POR HORA SON:");
+        System.out.println("--------------------------------");
+
         for (Empleado empleado : objetos) {
-            System.out.println("Los empleados por hora son:");
-            if (empleado instanceof EmpleadoPorHoras) {
-                EmpleadoPorHoras empleadoPorHoras = (EmpleadoPorHoras) empleado;
-                System.out.println("Trabajador por horas:");
-                System.out.println("Nombre: " + empleadoPorHoras.getNombre());
-                System.out.println("Salario: " + empleadoPorHoras.getSalario());
-                System.out.println("Horas trabajadas: " + empleadoPorHoras.getHorasTrabajadas());
-                System.out.println("Tarifa por hora: " + empleadoPorHoras.getTarifaHora());
+            if (empleado instanceof EmpleadoPorHoras empleadoPorHoras) {
+                System.out.println("\n---Trabajador por horas---");
+                System.out.println(" Nombre:               " + empleadoPorHoras.getNombre());
+                System.out.println(" Salario:              " + empleadoPorHoras.getSalario());
+                System.out.println(" Horas trabajadas:     " + empleadoPorHoras.getHorasTrabajadas());
+                System.out.println(" Tarifa por hora:      " + empleadoPorHoras.getTarifaHora());
             }
         }
 
         // obtener un empleado especifico
+        System.out.println("\n--------------------------------");
+        System.out.println("OBTENER UN EMPLEADO ESPECIFICO:");
+        System.out.println("--------------------------------");
+
         System.out.println("De cual trabajador desea obtener su informacion? ");
         System.out.print("  Ingese el numero de trabajador: ");
         int opcion = input.nextInt();
 
         if (opcion < 0 || opcion >= objetos.size()) {
-            System.out.println("Número de trabajador fuera de rango. Por favor, ingrese un número válido.");
+            System.out.println("Numero de trabajador fuera de rango. Por favor, ingrese un numero valido.");
         } else {
             System.out.println("\nLa informacion del trabajador " + num + " es:");
-            System.out.println("Nombre: " + objetos.get(num).getNombre());
-            System.out.println("Salario " + objetos.get(num).getSalario());
+            System.out.println("Nombre:             " + objetos.get(num).getNombre());
+            System.out.println("Salario:            " + objetos.get(num).getSalario());
+            System.out.println("Salario Calculado:  " + objetos.get(num).calcularSalario());
         }
 
     }
